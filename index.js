e5dff96e37eadae4203b93cd9295c963bab73553
Lizz <lizwesterband@gmail.com>
2020-08-08 13:29:09 -0700

    [files] Add new files

--- index.js
+++ index.js
@@ -9,17 +9,30 @@ app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static("public"))
 
 // GET - main index of site
-app.get('/', function(req, res) {
-  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+app.get('/',(req, res)=> {
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
   // Use request to call the API
-  axios.get(pokemonUrl).then(response => {
-    let pokemon = response.data.results;
-    res.render('index', { pokemon: pokemon.slice(0, 151) });
-  });
+  axios.get(pokemonUrl)
+  .then(response => {
+  console.log(response.data);
+  let pokemon = response.data.results;
+  res.render('index', { pokemon: pokemon.slice(0, 151) });
+});
 });
 
+
+app.get('/favorites',(req, res)=> {
+  let pokemonUrl = 'http://pokeapi.co/api/v2/pokemon?limit=151/';
+  // Use request to call the API
+  axios.get(pokemonUrl).then(response => {
+    console.log(response.data)
+    let pokemon = response.data.results;
+    res.render('favorites', { pokemon: pokemon.slice(0, 151) });
+  });
+});
 // Imports all routes from the pokemon routes file
 app.use('/pokemon', require('./routes/pokemon'));
 
